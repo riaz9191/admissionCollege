8a66f3d91dba97ebb05b9fbc23e99c98ed133b09
riaz9191 <riazahammed9191@gmail.com>
2023-07-23 16:26:22 +0600

    banner

--- admissionCollege/src/main.jsx
+++ admissionCollege/src/main.jsx
@@ -1,13 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./assets/Routes/Routes.jsx";
+import React from 'react';
+import './index.css';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
+import { Toaster } from 'react-hot-toast';
+import Main from './layout/Main';
+import Error from './pages/Error/Error';
+import Home from './pages/Home/Home';
+import AllCollege from './pages/College/AllCollege';
+import Login from './pages/Authentication/Login';
+import AuthProvider from './context/AuthProvider';
+import CollegeDetail from './pages/College/CollegeDetail';
+import PrivateRoute from './routes/PrivateRoute';
+import Admission from './pages/Admission/Admission';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <Error/>,
+    children: [
+     {path: '/', element:<Home/>},
+      { path: '/college', element: <AllCollege /> },
+      { path: '/admission', element: <Admission /> },
+      { path: '/details/:id', element: <PrivateRoute><CollegeDetail /></PrivateRoute>, loader: ({ params }) => fetch(`http://localhost:5000/colleges/${params.id}`) },
+
+
+      { path: '/login', element: <Login /> },
+   ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className="max-w-7xl mx-auto">
-      <RouterProvider router={router} />
-    </div>
-  </React.StrictMode>
+    <AuthProvider>
+      <HelmetProvider>
+        <Toaster />
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </AuthProvider>
+  </React.StrictMode>,
 );
